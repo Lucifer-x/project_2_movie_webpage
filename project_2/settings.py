e1503e8077602eaaab4ea7f285b7b9633bba42d9
Lucifer-x <46569317+Lucifer-x@users.noreply.github.com>
2022-10-26 13:54:45 +0800

    Update settings.py
    Fixed static root address

--- project_2/settings.py
+++ project_2/settings.py
@@ -114,10 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/').replace('\\', '/'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, '../collected_static')
+STATIC_ROOT = '/root/project_2/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
